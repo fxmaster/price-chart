d0dd5bcf5ae3d34f432a5d5e22703e8b59d5a0a4
Salamat Tulkibayev <fxmaster@tuta.io>
2022-04-16 02:36:25 +0600

    Dev: upd main.go.

--- service/parser/cmd/main.go
+++ service/parser/cmd/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"os"
+	"os/signal"
+	"price-chart/service/parser/internal/app"
+)
+
+func init() {
+	app.InitEnv(".env")
+}
 
 func main() {
-	fmt.Println("hello world")
+	sigs := make(chan os.Signal, 1)
+	defer close(sigs)
+
+	done := make(chan struct{})
+	defer close(done)
+
+	signal.Notify(sigs, os.Interrupt)
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func() {
+		app.Run(ctx)
+		done <- struct{}{}
+	}()
+
+	<-sigs
+	cancel()
+	<-done
 }
